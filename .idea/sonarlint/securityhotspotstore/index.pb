4b622af4416194a0fc63751ce28bf8d1ff7695c9
hyoungj00n <khjoon372@naver.com>
2024-01-15 03:33:52 +0900

    [Init] 프로젝트 생성

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+g
+7src/main/java/com/umc/TheGoods/TheGoodsApplication.java,8/b/8b69f7819d677e7684306868d80fd1d073b5f5a5
---
+++ src/main/java/com/umc/TheGoods/TheGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.umc.TheGoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGoodsApplication.class, args);
+	}
+
+}
