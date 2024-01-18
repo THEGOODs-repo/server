18564ac8d17d99946fed02b74d5412101cffc251
yxhwxn <officeyh9819@gmail.com>
2024-01-19 03:39:47 +0900

    chore: package name 수정

--- src/main/java/com/umc/thegoods/TheGoodsApplication.java
+++ src/main/java/com/umc/thegoods/TheGoodsApplication.java
@@ -1,4 +1,4 @@
-package com.umc.TheGoods;
+package com.umc.thegoods;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TheGoodsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TheGoodsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TheGoodsApplication.class, args);
+    }
 
 }
--- src/test/java/com/umc/thegoods/TheGoodsApplicationTests.java
+++ src/test/java/com/umc/thegoods/TheGoodsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.umc.TheGoods;
+package com.umc.thegoods;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TheGoodsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
