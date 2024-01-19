f10090a5ab6d3feed8a23004e365c83fb9726f5a
Eunjin Kim <jennyeunjin@khu.ac.kr>
2024-01-19 17:15:54 +0900

    :sparkles: feat: 도메인(상품, 주문) 클래스 추가

---
+++ src/main/java/com/umc/thegoods/domain/Cart.java
@@ -0,0 +1,6 @@
+package com.umc.thegoods.domain;
+
+import com.umc.thegoods.domain.common.BaseDateTimeEntity;
+
+public class Cart extends BaseDateTimeEntity {
+}
---
+++ src/main/java/com/umc/thegoods/domain/images/ItemImg.java
@@ -0,0 +1,6 @@
+package com.umc.thegoods.domain.images;
+
+import com.umc.thegoods.domain.common.BaseDateTimeEntity;
+
+public class ItemImg extends BaseDateTimeEntity {
+}
