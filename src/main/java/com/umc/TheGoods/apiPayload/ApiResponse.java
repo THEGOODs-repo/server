990d574bd3e75dcb1427f987f706c6aeacfe2bbc
gyu_hyeok <jjookki33@gmail.com>
2024-01-20 02:38:39 +0900

    :sparkles: Feat: api 응답 통일, 에러 처리 구현
    api 응답 형태 구현

---
+++ src/main/java/com/umc/TheGoods/apiPayload/ApiResponse.java
@@ -0,0 +1,33 @@
+package com.umc.TheGoods.apiPayload;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.umc.TheGoods.apiPayload.code.BaseCode;
+import com.umc.TheGoods.apiPayload.code.status.SuccessStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+
+    @JsonPropertyOrder("isSuccess")
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
+
+    public static <T> ApiResponse<T> onSuccess(T result) {
+        return new ApiResponse<>(true, SuccessStatus._OK.getCode(), SuccessStatus._OK.getMessage(), result);
+    }
+
+    public static <T> ApiResponse<T> of(BaseCode code, T result) {
+        return new ApiResponse<>(true, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(), result);
+    }
+
+    public static <T> ApiResponse<T> onFailure(String code, String message, T data) {
+        return new ApiResponse<>(false, code, message, data);
+    }
+}
