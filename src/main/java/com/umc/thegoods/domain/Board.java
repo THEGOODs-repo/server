7b3d763731d9ad5041ed63648ffb16d1750ba18f
yxhwxn <officeyh9819@gmail.com>
2024-01-20 23:07:40 +0900

    :sparkles: feat: 엔티티 기본 컬럼 추가(문의 게시판)

--- src/main/java/com/umc/thegoods/domain/Board.java
+++ src/main/java/com/umc/thegoods/domain/Board.java
@@ -1,6 +1,32 @@
 package com.umc.thegoods.domain;
 
 import com.umc.thegoods.domain.common.BaseDateTimeEntity;
+import com.umc.thegoods.domain.enums.BoardStatus;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "board")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 생성 로직 규정
 public class Board extends BaseDateTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "board_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(10)", nullable = false)
+    private BoardStatus status;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private Users user;
+
+//    @JoinColumn(name = "item_id")
+//    private Item item;
 }
