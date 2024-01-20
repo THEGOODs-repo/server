11eddc7e20fa73a8966841c4bed42f59145f50bb
yxhwxn <officeyh9819@gmail.com>
2024-01-20 23:07:53 +0900

    :sparkles: feat: 엔티티 기본 컬럼 추가(포스트-댓글)

--- src/main/java/com/umc/thegoods/domain/Comment.java
+++ src/main/java/com/umc/thegoods/domain/Comment.java
@@ -1,6 +1,30 @@
 package com.umc.thegoods.domain;
 
 import com.umc.thegoods.domain.common.BaseDateTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 생성 로직 규정
 public class Comment extends BaseDateTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+
+    /**
+     * TODO
+     * 대댓글 관련 parrentId 처리 필요
+     * admin 엔티티 처리(회원 도메인과 상의)
+     */
+    private Long parrentId;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
 }
