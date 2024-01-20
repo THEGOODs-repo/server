cb35ff1510108d5f206c567fbec32c4e3d6a247c
yxhwxn <officeyh9819@gmail.com>
2024-01-20 23:13:53 +0900

    :sparkles: feat: 엔티티 기본 컬럼 추가(Post-community)

--- src/main/java/com/umc/thegoods/domain/Post.java
+++ src/main/java/com/umc/thegoods/domain/Post.java
@@ -1,6 +1,28 @@
 package com.umc.thegoods.domain;
 
 import com.umc.thegoods.domain.common.BaseDateTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 생성 로직 규정
 public class Post extends BaseDateTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = true)
+    private String content;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private Users user;
+
 }
