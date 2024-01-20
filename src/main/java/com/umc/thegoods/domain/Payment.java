864386748895ae48a126d7a6016d7738f2eec2e4
yxhwxn <officeyh9819@gmail.com>
2024-01-20 23:14:36 +0900

    :sparkles: feat: 결제 도메인 추가

--- src/main/java/com/umc/thegoods/domain/Payment.java
+++ src/main/java/com/umc/thegoods/domain/Payment.java
@@ -1,6 +1,20 @@
 package com.umc.thegoods.domain;
 
 import com.umc.thegoods.domain.common.BaseDateTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "payment")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 생성 로직 규정
 public class Payment extends BaseDateTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "payment_id")
+    private Long id;
 }
