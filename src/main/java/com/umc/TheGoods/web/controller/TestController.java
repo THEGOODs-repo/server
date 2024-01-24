153c874407eafec6cd1fc6c84d5b082897453467
yxhwxn <officeyh9819@gmail.com>
2024-01-25 04:12:13 +0900

    :sparkles: Feat: add swagger Test API

---
+++ src/main/java/com/umc/TheGoods/web/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.umc.thegoods.web.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class TestController {
+
+    @GetMapping("/test/hello")
+    @Operation(summary = "Return hello", description = "simple API for swagger test!")
+    public String hello() {
+        return "Hello, Swagger!";
+    }
+}
