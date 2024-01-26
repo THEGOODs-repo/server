530b521a2258f6101e939757063d12cb16bfbd14
Eunjin Kim <jennyeunjin@khu.ac.kr>
2024-01-27 01:04:56 +0900

    :recycle: Refactor: test application 패키지명 수정

--- src/test/java/com/umc/TheGoods/TheGoodsApplicationTests.java
+++ src/test/java/com/umc/TheGoods/TheGoodsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.umc.thegoods;
+package com.umc.TheGoods;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class thegoodsApplicationTests {
+class ThegoodsApplicationTests {
 
     @Test
     void contextLoads() {
