aef26185af3a73eaa7f78268441c593761154cab
GyuHyeokjjookki <jjookki33@gmail.com>
2024-01-26 18:06:45 +0900

    :sparkles: Feat: 상품 등록 API 구현
    item 등록을 위한 controller 구현

---
+++ src/main/java/com/umc/TheGoods/web/controller/ItemRestController.java
@@ -0,0 +1,14 @@
+package com.umc.TheGoods.web.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@RequestMapping("/api")
+public class ItemRestController {
+}
