50137a34b6fb376c9913eaa508ec59bca5a3a925
GyuHyeokjjookki <jjookki33@gmail.com>
2024-01-29 16:59:16 +0900

    :sparkles: Feat: 상품 등록 API 구현
    상품 등록 controller 함수 구현

--- src/main/java/com/umc/TheGoods/web/controller/ItemRestController.java
+++ src/main/java/com/umc/TheGoods/web/controller/ItemRestController.java
@@ -1,14 +1,41 @@
 package com.umc.TheGoods.web.controller;
 
 
+import com.umc.TheGoods.apiPayload.ApiResponse;
+import com.umc.TheGoods.apiPayload.code.status.ErrorStatus;
+import com.umc.TheGoods.apiPayload.exception.handler.MemberHandler;
+import com.umc.TheGoods.converter.Item.ItemConverter;
+import com.umc.TheGoods.domain.item.Item;
+import com.umc.TheGoods.domain.member.Member;
+import com.umc.TheGoods.service.ItemService.ItemCommandService;
+import com.umc.TheGoods.service.MemberService.MemberQueryService;
+import com.umc.TheGoods.web.dto.ItemRequestDTO;
+import com.umc.TheGoods.web.dto.ItemResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.Authentication;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @Validated
 @RequestMapping("/api")
 public class ItemRestController {
+
+    private final ItemCommandService itemCommandService;
+    private final MemberQueryService memberQueryService;
+
+    @PostMapping("/seller/item")
+    public ApiResponse<ItemResponseDTO.UploadItemResultDTO> upload(@RequestBody @Valid ItemRequestDTO.UploadItemDTO request,
+                                                                   @RequestParam Long memberId,
+                                                                   Authentication authentication){
+
+        //MemberDetail memberDetail = (MemberDetail) authentication.getPrincipal();
+        Member member = memberQueryService.findMemberById(memberId).orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        Item item = itemCommandService.uploadItem(member,request);
+        return ApiResponse.onSuccess(ItemConverter.toUploadItemResultDTO(item));
+    }
 }
