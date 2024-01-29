b7c5d738db6578d0e9afd0e473547833363a2418
GyuHyeokjjookki <jjookki33@gmail.com>
2024-01-29 16:54:48 +0900

    :sparkles: Feat: 상품 등록 API 구현
    상품 등록을 위한 member 검증 기능 구현

---
+++ src/main/java/com/umc/TheGoods/service/MemberService/MemberQueryService.java
@@ -0,0 +1,9 @@
+package com.umc.TheGoods.service.MemberService;
+
+import com.umc.TheGoods.domain.member.Member;
+
+import java.util.Optional;
+
+public interface MemberQueryService {
+    Optional<Member> findMemberById(Long id);
+}
---
+++ src/main/java/com/umc/TheGoods/service/MemberService/MemberQueryServiceImpl.java
@@ -0,0 +1,21 @@
+package com.umc.TheGoods.service.MemberService;
+
+import com.umc.TheGoods.domain.member.Member;
+import com.umc.TheGoods.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberQueryServiceImpl implements MemberQueryService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Optional<Member> findMemberById(Long id) {
+        return memberRepository.findById(id);
+    }
+}
