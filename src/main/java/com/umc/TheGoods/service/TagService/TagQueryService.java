4b4dbf6d0fa6bb62f531acfc1864cb174c846d01
GyuHyeokjjookki <jjookki33@gmail.com>
2024-01-30 14:33:32 +0900

    :sparkles: Feat: 상품 등록 API 구현
    tag를 찾는 service 함수 구현

---
+++ src/main/java/com/umc/TheGoods/service/TagService/TagQueryService.java
@@ -0,0 +1,8 @@
+package com.umc.TheGoods.service.TagService;
+
+import com.umc.TheGoods.repository.TagRepository;
+
+public interface TagQueryService {
+
+    public boolean existTagById(Long id);
+}
