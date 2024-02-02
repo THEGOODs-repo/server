61dac749c8cd4764359d9869c318953a29fbe236
GyuHyeokjjookki <jjookki33@gmail.com>
2024-02-02 17:56:21 +0900

    :sparkles: Feat: 상품 정보 수정 API & 상품 상세 정보 조회 API 구현
    item 상세 조회를 위한 controller 일차 구현

--- src/main/java/com/umc/TheGoods/web/controller/ItemRestController.java
+++ src/main/java/com/umc/TheGoods/web/controller/ItemRestController.java
@@ -18,10 +18,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -49,5 +46,11 @@ public class ItemRestController {
 
         Item item = itemCommandService.uploadItem(member, request);
         return ApiResponse.onSuccess(ItemConverter.toUploadItemResultDTO(item));
+    }
+
+    @GetMapping("/seller/item/{itemId}")
+    public ApiResponse<ItemResponseDTO.ItemContentDTO> getPostContent(@PathVariable(name = "itemId") Long itemId) {
+        Item itemContent = itemCommandService.getItemContent(itemId);
+        return ApiResponse.onSuccess(ItemConverter.getItemContentDTO(itemContent));
     }
 }
