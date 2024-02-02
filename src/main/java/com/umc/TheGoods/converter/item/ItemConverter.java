ce52dc163e0523ebd842227bc98f43a8243fb3e6
GyuHyeokjjookki <jjookki33@gmail.com>
2024-02-02 17:54:56 +0900

    :sparkles: Feat: 상품 정보 수정 API & 상품 상세 정보 조회 API 구현
    item 상세 조회를 위한 converter 구현

--- src/main/java/com/umc/TheGoods/converter/item/ItemConverter.java
+++ src/main/java/com/umc/TheGoods/converter/item/ItemConverter.java
@@ -1,15 +1,64 @@
 package com.umc.TheGoods.converter.item;
 
 import com.umc.TheGoods.domain.enums.ItemStatus;
+import com.umc.TheGoods.domain.images.ItemImg;
 import com.umc.TheGoods.domain.item.Item;
+import com.umc.TheGoods.domain.item.ItemOption;
 import com.umc.TheGoods.domain.types.DeliveryType;
 import com.umc.TheGoods.web.dto.item.ItemRequestDTO;
 import com.umc.TheGoods.web.dto.item.ItemResponseDTO;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ItemConverter {
+
+    public static ItemResponseDTO.ItemImgResponseDTO getItemImgDTO(ItemImg itemImg) {
+        return ItemResponseDTO.ItemImgResponseDTO.builder()
+                .isThumbNail(itemImg.getThumbnail())
+                .itemImgUrl(itemImg.getUrl())
+                .build();
+    }
+
+    public static ItemResponseDTO.ItemOptionResponseDTO getItemOptionDTO(ItemOption itemOption) {
+        return ItemResponseDTO.ItemOptionResponseDTO.builder()
+                .itemOptionId(itemOption.getId())
+                .name(itemOption.getName())
+                .stock(itemOption.getStock())
+                .price(itemOption.getPrice())
+                .build();
+    }
+
+    public static ItemResponseDTO.ItemContentDTO getItemContentDTO(Item item) {
+        List<ItemResponseDTO.ItemImgResponseDTO> itemImgResponseDTOList = item.getItemImgList().stream()
+                .map(ItemConverter::getItemImgDTO).collect(Collectors.toList());
+
+        List<ItemResponseDTO.ItemOptionResponseDTO> itemOptionResponseDTOList = item.getItemOptionList().stream()
+                .map(ItemConverter::getItemOptionDTO).collect(Collectors.toList());
+
+        return ItemResponseDTO.ItemContentDTO.builder()
+                .itemId(item.getId())
+                .category(item.getCategory().getName())
+                .deliveryDate(item.getDeliveryDate())
+                .deliveryFee(item.getDeliveryFee())
+                .deliveryType(item.getDeliveryType())
+                .status(item.getStatus())
+                .isLimitless(item.getIsLimitless())
+                .stock(item.getStock())
+                .price(item.getPrice())
+                .description(item.getDescription())
+                .startDate(item.getStartDate())
+                .endDate(item.getEndDate())
+                .salesCount(item.getSalesCount())
+                .viewCount(item.getViewCount())
+                .dibsCount(item.getDibsCount())
+                .itemTag(item.getItemTagList().stream().map(tag -> tag.getTag().getName()).toList())
+                .itemImgUrlList(itemImgResponseDTOList)
+                .itemOptionList(itemOptionResponseDTOList)
+                .build();
+    }
 
     public static ItemResponseDTO.UploadItemResultDTO toUploadItemResultDTO(Item item) {
         return ItemResponseDTO.UploadItemResultDTO.builder()
