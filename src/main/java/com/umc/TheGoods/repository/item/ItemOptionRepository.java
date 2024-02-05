8df5ec4ba78bc3d88301b130874e8d3072cc91a7
gyu_hyeok <jjookki33@gmail.com>
2024-02-06 02:52:16 +0900

    :sparkles: Feat: 상품 정보 수정 API & 상품 상세 정보 조회 API 구현
    item update를 위한 repository 구현

--- src/main/java/com/umc/TheGoods/repository/item/ItemOptionRepository.java
+++ src/main/java/com/umc/TheGoods/repository/item/ItemOptionRepository.java
@@ -3,5 +3,9 @@ package com.umc.TheGoods.repository.item;
 import com.umc.TheGoods.domain.item.ItemOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemOptionRepository extends JpaRepository<ItemOption, Long> {
+
+    List<ItemOption> findByItemId(Long itemId);
 }
