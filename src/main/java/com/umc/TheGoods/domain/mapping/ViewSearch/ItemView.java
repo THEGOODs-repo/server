94ce3346457b3dab4a4d7899cd3fa470936c1829
GyuHyeokjjookki <jjookki33@gmail.com>
2024-02-05 18:08:02 +0900

    :sparkles: Feat: 상품 정보 수정 API & 상품 상세 정보 조회 API 구현
    itemview와 item mapping 추가

--- src/main/java/com/umc/TheGoods/domain/mapping/ViewSearch/ItemView.java
+++ src/main/java/com/umc/TheGoods/domain/mapping/ViewSearch/ItemView.java
@@ -26,4 +26,18 @@ public class ItemView extends BaseDateTimeEntity {
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "item_id", nullable = false)
     private Item item;
+
+    public void setMember(Member member) {
+        if (this.member != null)
+            member.getItemViewList().remove(this);
+        this.member = member;
+        member.getItemViewList().add(this);
+    }
+
+    public void setItem(Item item) {
+        if (this.item != null)
+            item.getItemViewList().remove(this);
+        this.item = item;
+        item.getItemViewList().add(this);
+    }
 }
