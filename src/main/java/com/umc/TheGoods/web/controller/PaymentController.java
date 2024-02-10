70689593f9c101fd262ab0ae856a73bc2d53efca
yxhwxn <officeyh9819@gmail.com>
2024-02-10 21:26:45 +0900

    :sparkles: Feat: 결제 API 개발 setup

--- src/main/java/com/umc/TheGoods/web/controller/PaymentController.java
+++ src/main/java/com/umc/TheGoods/web/controller/PaymentController.java
@@ -1,11 +1,17 @@
 package com.umc.TheGoods.web.controller;
 
+import com.umc.TheGoods.domain.Payment;
+import com.umc.TheGoods.web.dto.payment.PaymentRequestDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @Slf4j
 @Tag(name = "Payment", description = "결제(포트원) 관련 API")
@@ -15,17 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/payment")
 public class PaymentController {
 
-//    @PostMapping("/payment/prepare")
-//    public void preparePayment(@RequestBody PrePaymentEntity request)
-//            throws IamportResponseException, IOException {
-//        paymentService.postPrepare(request);
-//    }
+    @PostMapping("/payment/prepare")
+    public void preparePayment(@RequestBody PaymentRequestDTO request)
+            throws IamportResponseException, IOException {
+        paymentService.postPrepare(request);
+    }
 
-//    @PostMapping("/api/v1/payments")
-//    public IamportResponse<Payment> paymentByImpUid(@RequestBody PaymentRequest request) throws
-//            IamportResponseException,
-//            IOException {
-//        log.info("Payment Request : {}", request.toString());
-//        return iamportClient.paymentByImpUid(request.impUid());
-//    }
+    @PostMapping("/payments")
+    public IamportResponse<Payment> paymentByImpUid(@RequestBody PaymentRequestDTO request) throws
+            IamportResponseException,
+            IOException {
+        log.info("Payment Request : {}", request.toString());
+        return iamportClient.paymentByImpUid(request.impUid());
+    }
 }
--- src/main/java/com/umc/TheGoods/web/dto/payment/PaymentRequestDTO.java
+++ src/main/java/com/umc/TheGoods/web/dto/payment/PaymentRequestDTO.java
@@ -1,4 +1,16 @@
 package com.umc.TheGoods.web.dto.payment;
 
-public class PaymentRequestDTO {
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PaymentRequesttDto {
+    private String orderUid;
+    private String itemName;
+    private String buyerName;
+    private Long paymentPrice;
+    private String buyerEmail;
+    private String buyerAddress;
 }
