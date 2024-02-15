2cb3db9abb75603034bda925930fc72c5f4d944f
yxhwxn <officeyh9819@gmail.com>
2024-02-16 01:23:26 +0900

    :bug: Fix: EC2 Configure 관련 충돌 해결

--- src/main/java/com/umc/TheGoods/TheGoodsApplication.java
+++ src/main/java/com/umc/TheGoods/TheGoodsApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication()
 @EnableJpaAuditing
 public class TheGoodsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(com.umc.TheGoods.TheGoodsApplication.class, args);
     }
-
 }
