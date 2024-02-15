d94a74af7eb979f312df1bc68fecd1346d3da264
Eunjin Kim <jennyeunjin@khu.ac.kr>
2024-02-16 01:00:06 +0900

    :bug: BugFix: CI/CD 오류 수정

--- src/main/java/com/umc/TheGoods/TheGoodsApplication.java
+++ src/main/java/com/umc/TheGoods/TheGoodsApplication.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = {
-        org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
-        org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
-        org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableJpaAuditing
 public class TheGoodsApplication {
 
