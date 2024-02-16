82606a7863579252bba1747f8690c407a966b15d
GyuHyeokjjookki <jjookki33@gmail.com>
2024-02-16 14:10:11 +0900

    :sparkles: Feat: 방금 전 본 상품과 유사한 상품 추천 기능 구현
    item view를 찾기 위한 repository code 구현

--- src/main/java/com/umc/TheGoods/repository/item/ItemViewRepository.java
+++ src/main/java/com/umc/TheGoods/repository/item/ItemViewRepository.java
@@ -3,5 +3,9 @@ package com.umc.TheGoods.repository.item;
 import com.umc.TheGoods.domain.mapping.ViewSearch.ItemView;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemViewRepository extends JpaRepository<ItemView, Long> {
+
+    List<ItemView> findAllByMemberIdOrderByCreatedAtDesc(Long memberId);
 }
