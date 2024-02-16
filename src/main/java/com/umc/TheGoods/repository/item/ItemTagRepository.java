80ee9489f3e3f72f34a69c81d4df3e13b3370342
GyuHyeokjjookki <jjookki33@gmail.com>
2024-02-16 17:31:08 +0900

    :sparkles: Feat: 태그가 많이 등록된 상품 조회 API 구현
    item tag가 많은 순으로 정렬하기 위한 item tag repository 구현

--- src/main/java/com/umc/TheGoods/repository/item/ItemTagRepository.java
+++ src/main/java/com/umc/TheGoods/repository/item/ItemTagRepository.java
@@ -1,7 +1,9 @@
 package com.umc.TheGoods.repository.item;
 
 import com.umc.TheGoods.domain.mapping.Tag.ItemTag;
+import com.umc.TheGoods.web.dto.item.ConsultResultDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,4 +12,10 @@ public interface ItemTagRepository extends JpaRepository<ItemTag, Long> {
     List<ItemTag> findByItemId(Long itemId);
 
     ItemTag findByTagId(Long tagId);
+
+    @Query("SELECT NEW com.umc.TheGoods.web.dto.item.ConsultResultDTO(it.item.id,COUNT(it.tag.id)) " +
+            "FROM ItemTag AS it " +
+            "GROUP BY it.item.id " +
+            "ORDER BY COUNT(it.tag.id) DESC")
+    List<ConsultResultDTO> countTagsByTagId();
 }
