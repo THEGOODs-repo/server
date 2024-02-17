e076a1d26b42463c05471c02457d5f7d249aa6e4
gyu_hyeok <jjookki33@gmail.com>
2024-02-18 01:02:22 +0900

    :sparkles: Feat: 상품 검색 정렬 기능 API 구현
    item search sort method repository code

--- src/main/java/com/umc/TheGoods/repository/item/ItemRepository.java
+++ src/main/java/com/umc/TheGoods/repository/item/ItemRepository.java
@@ -6,6 +6,8 @@ import com.umc.TheGoods.domain.member.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,4 +25,25 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     //Page<Item> findAllByItemTagListIn(List<ItemTag> itemTagList, PageRequest pageRequest);
 
     Page<Item> findAllByEndDateGreaterThanEqual(LocalDate localDate, PageRequest pageRequest);
+
+    @Query("SELECT i FROM Item i LEFT JOIN i.reviewList r WHERE i.category.name = :categoryName GROUP BY i.id ORDER BY COUNT(r) DESC")
+    Page<Item> findAllByCategoryNameOrderByReviewListSizeDesc(@Param("categoryName") String categoryName, PageRequest pageRequest);
+
+    @Query("SELECT i FROM Item i LEFT JOIN i.reviewList r WHERE i.name LIKE %:itemName% GROUP BY i.id ORDER BY COUNT(r) DESC")
+    Page<Item> findAllByItemNameContainingOrdOrderByReviewListSizeDesc(@Param("itemName") String itemName, PageRequest pageRequest);
+
+    @Query("SELECT i FROM Item i LEFT JOIN i.reviewList r WHERE i.member.nickname = :memberName GROUP BY i.id ORDER BY COUNT(r) DESC")
+    Page<Item> findAllBySellerNameOrderByReviewListSizeDesc(@Param("memberName") String memberName, PageRequest pageRequest);
+
+    @Query("SELECT i FROM Item i " +
+            "LEFT JOIN i.itemTagList itl " +
+            "LEFT JOIN i.reviewList r " +
+            "WHERE EXISTS (" +
+            "   SELECT 1 FROM ItemTag it " +
+            "   WHERE it.tag IN :tags " +
+            "   AND it.item.id = i.id" +
+            ") " +
+            "GROUP BY i.id " +
+            "ORDER BY COUNT(r) DESC")
+    List<Item> findAllByItemTagListTagInOrderByReviewListSizeDesc(@Param("tags") List<Tag> tags);
 }
